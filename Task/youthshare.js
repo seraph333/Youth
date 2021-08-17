b7a02d9e45391b74da0647e97749e770dced1896
Xin-Qiang <48395727+Xin-Qiang@users.noreply.github.com>
2021-08-18 07:18:33 +0800

    Update youthshare.js

--- Task/youthshare.js
+++ Task/youthshare.js
@@ -2,7 +2,7 @@ const $ = new Env('youthshare');
 let md5 = require('md5-node');
 let nowTime;
 let wxck;
-let articles = ["https://focus.youth.cn/article/s?signature=X6AKVevx2zmNQOjaBJBZQmhZ6MlJF6oq26Ead93krDoJqw0WYn&uid=48317856&phone_code=29a522d895c83da15782b837e67e366d&scid=39717432&time=1629156027&app_version=2.0.0&sign=74513081b33a328bb96b3c227d072e75"
+let articles = ["https://focus.youth.cn/article/s?signature=ML5JYgKNrewq9QO4gqQXg6U36lOzteyGBQpayGDn2Z0PAvkopB&uid=48317856&phone_code=29a522d895c83da15782b837e67e366d&scid=39731300&time=1629242263&app_version=2.0.0&sign=1107c05d6a5a10612b1cea80031a5534"
 ]
 
 let encodearticles;
